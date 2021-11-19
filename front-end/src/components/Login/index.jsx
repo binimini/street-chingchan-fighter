8921876a0577da2391c265d944cb4d359bd6bdd4
jeongbbn <kkaatteelyn@gmail.com>
2021-11-20 04:49:53 +0900

    [ADD] 모든 화면 구현 및 이벤트 처리 완료

--- front-end/src/components/Login/index.jsx
+++ front-end/src/components/Login/index.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.scss";
 
 export default function Login() {
   const [avatarIdx, setAvatarId] = useState(0);
   const MAX_AVATARIDX = 5;
+  const canvasContainer = useRef();
+  const nickNameRef = useRef();
+
+  const submit = () => {
+    console.log(nickNameRef.current.value);
+    //
+  };
+
+  useEffect(() => {
+    const canvas = canvasContainer.current;
+    const ctx = canvas.getContext("2d");
+    const player = new Image();
+    player.src = `/images/avatar/avatar${avatarIdx + 1}.png`;
+    ctx.clearRect(0, 0, 32, 32);
+    player.onload = () => {
+      ctx.drawImage(player, 0, 0, 32, 32, 0, 0, 32, 32);
+    };
+    return () => {};
+  }, [avatarIdx]);
 
   const changeAvartarId = (diff) => {
     const nowIdx = avatarIdx + diff;
@@ -22,7 +41,7 @@ export default function Login() {
           onClick={() => changeAvartarId(-1)}
         >{`<`}</div>
         <div className="login__image">
-          <canvas width="100" height="130"></canvas>
+          <canvas width="32" height="32" ref={canvasContainer}></canvas>
         </div>
         <div
           className="login__image__arrow"
@@ -31,10 +50,14 @@ export default function Login() {
       </div>
       <div className="login__nickname__container">
         <input
+          ref={nickNameRef}
           className="login__nickname__input"
           placeholder="닉네임을 입력해주세요."
         />
       </div>
+      <button className="login__button" onClick={submit}>
+        배틀장 입장
+      </button>
     </div>
   );
 }
