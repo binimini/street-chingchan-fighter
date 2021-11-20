7a0267b05948c14f086747604dcafefc3f348318
Subin Min <tnqls5417@naver.com>
2021-11-20 13:49:48 +0900

    [ADD] 게임 스토어 저장 로직 수정 및 싸움 준비로 타이머 시작 구현

--- front-end/src/components/Town/TownMap/Map/index.js
+++ front-end/src/components/Town/TownMap/Map/index.js
@@ -9,8 +9,8 @@ const drawMap = () => {
   const mapCanvas = document.getElementById("map-canvas");
   const ctx = mapCanvas.getContext("2d");
   const img = new Image();
-  
-  img.src = '/images/map/map.png';
+
+  img.src = "/images/map/map.png";
   img.onload = function () {
     mapCanvas.width = img.naturalWidth;
     mapCanvas.height = img.naturalHeight;
@@ -27,14 +27,12 @@ const Map = () => {
   }, []);
 
   useEffect(() => {
-    if(!userList) return;
-
+    if (!userList) return;
     userList.forEach((u) => {
-      if(!u.nickname) return;
-      if(u.id === socketClient.id) return;
-
+      if (!u.nickname) return;
+      if (u.id === socketClient.id) return;
       const avatarCanvas = document.querySelector(`#${u.id}`);
-      const ctx = avatarCanvas.getContext('2d');
+      const ctx = avatarCanvas.getContext("2d");
       const img = new Image();
 
       img.src = `/images/avatar/avatar${u.avatarIdx}.png`;
@@ -42,12 +40,19 @@ const Map = () => {
         avatarCanvas.width = img.naturalWidth / 16 + 20;
         avatarCanvas.height = img.naturalHeight + 20;
 
-        avatarCanvas.closest('div').style.transform = `translate(${u.x}px,${u.y}px)`;
-        drawAvatar(ctx, img, u.avatarSrcPosition, avatarCanvas.width, avatarCanvas.height);
-      }
+        avatarCanvas.closest(
+          "div"
+        ).style.transform = `translate(${u.x}px,${u.y}px)`;
+        drawAvatar(
+          ctx,
+          img,
+          u.avatarSrcPosition,
+          avatarCanvas.width,
+          avatarCanvas.height
+        );
+      };
     });
-
-  }, [userList])
+  }, [userList]);
 
   return (
     <>
@@ -55,15 +60,14 @@ const Map = () => {
         <canvas id="map-canvas"></canvas>
         <Avatar />
         {userList.map((u) => {
-          if(!u.nickname) return;
-          if(u.id === socketClient.id) return;
-          
+          if (!u.nickname) return;
+          if (u.id === socketClient.id) return;
           return (
             <div className="avatar" key={u.id}>
               <canvas id={u.id}></canvas>
               <p>{u.nickname}</p>
             </div>
-          )
+          );
         })}
       </div>
     </>
