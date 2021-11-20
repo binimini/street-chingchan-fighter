b12ace10f6ae6297d8433a3f10f1975d58844978
24to26 <ckdwhzjarhk@korea.ac.kr>
2021-11-20 12:46:26 +0900

    [ADD] appbar 및 게임 화면 스타일 정리

--- front-end/src/components/Stage/index.jsx
+++ front-end/src/components/Stage/index.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect } from "react";
 import { useSocket, useSocketData } from "../../context/SocketContext";
+import MessageBox from "../MessageBox";
 import "./style.scss";
 
 function Stage() {
@@ -43,18 +44,38 @@ function Stage() {
         <div></div>
       ) : (
         <div className="room__container">
-          {roomMembers.length > 0 &&
-            roomMembers.map((id) => <div key={id}>{id}</div>)}
+          <div className="room__title">* 받고싶은 칭찬을 골라주세요 *</div>
+          <div className="room__content">
+            <div className="message_box__container">
+              <MessageBox>
+                <p>
+                  어떤 칭찬이{" "}
+                  {
+                    roomMembers.filter(({ id }) => id !== socketClient.id)[0]
+                      .nickname
+                  }
+                  님을 행복하게 만들까요?
+                </p>
+                <p>ヽ(✿ﾟ▽ﾟ)ノ</p>
+              </MessageBox>
+            </div>
+            {roomMembers.length > 0 &&
+              roomMembers.map(({ id, nickname }) => (
+                <div key={id}>
+                  {id} {nickname}
+                </div>
+              ))}
 
-          <button
-            className="room__start_btn"
-            disabled={!canStart}
-            onClick={handleFightStart}
-          >
-            Start
-          </button>
-          <div>시간 : {roomTime}</div>
-          <div>room# : {roomID}</div>
+            <button
+              className="room__start_btn"
+              disabled={!canStart}
+              onClick={handleFightStart}
+            >
+              Start
+            </button>
+            <div>시간 : {roomTime}</div>
+            <div>room# : {roomID}</div>
+          </div>
         </div>
       )}
       {/* {userList.map(({ id }) => (
