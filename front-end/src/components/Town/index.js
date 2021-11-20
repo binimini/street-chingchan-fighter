18f31e509face25072fb1fdf8ae191764b9ae2dc
24to26 <ckdwhzjarhk@korea.ac.kr>
2021-11-20 11:39:46 +0900

    [ADD] 스테이지 컴포넌트 적용

--- front-end/src/components/Town/index.js
+++ front-end/src/components/Town/index.js
@@ -1,17 +1,15 @@
-import { useEffect } from "react";
-import { useSocket } from "../../context/SocketContext";
 import TownMap from "./TownMap";
 
-import './style.scss';
+import "./style.scss";
 
 const Town = () => {
   return (
     <>
-      <div className={'town-map'}>
+      <div className={"town-map"}>
         <TownMap />
       </div>
     </>
-  )
+  );
 };
 
 export default Town;
